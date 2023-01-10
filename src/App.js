0d12635d283c94446321cc8c8cfb9ab2b6c3998c
RafiUddin <rafiuddin.cse.pciu@gmail.com>
2023-01-10 22:59:18 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Product from "./components/Product/Product";
+import Shop from "./components/Shop/Shop";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Shop></Shop>
+      <Product></Product>
+    </div>
+  );
+}
+
+export default App;
